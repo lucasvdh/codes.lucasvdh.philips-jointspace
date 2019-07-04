fa1688610faef87eb02e61ce4d3fcb304e45cc14
Lucas van der Have <lucasvdhave@gmail.com>
2019-07-04 22:55:17 +0200

    :art: Extend pairing test script with all variations on pairing

--- tests/pairing.js
+++ tests/pairing.js
@@ -17,7 +17,25 @@ let questions = [{
         message: "What port number should be used? Either 1925 or 1926",
     }],
     deviceId = 'testing',
-    client = new JointspaceClient(deviceId);
+    client = new JointspaceClient(deviceId),
+    pairingVariations = [
+        {
+            port: 1926,
+            secure: true
+        },
+        {
+            port: 1926,
+            secure: false
+        },
+        {
+            port: 1925,
+            secure: true
+        },
+        {
+            port: 1925,
+            secure: false
+        }
+    ];
 
 inquirer.prompt(questions).then(answers => {
     console.log('Updating _client config');
@@ -25,10 +43,60 @@ inquirer.prompt(questions).then(answers => {
     client.setSecure(false);
     client.setPort(answers.port);
 
-    // _client.debug = true;
+    let onResolvePair = (response) => {
+        if (response.error_id === 'SUCCESS') {
+            console.log('Successfully started pairing process');
 
-    console.log('Sending system info call');
-    client.getSystem().then((response) => {
+            inquirer.prompt([{
+                'type': 'input',
+                'name': 'pin',
+                'message': 'What\'s the pin code displayed on the tv?'
+            }]).then(answers => {
+                console.log('Sending pin verification:', answers.pin);
+
+                client.confirmPair(answers.pin).then((credentials) => {
+                    console.log('Pairing process completed successfully!');
+                    console.log('Your tv is now accessible with the following digest credentials', credentials);
+                }).catch((error) => {
+                    if (typeof error.error_id !== "undefined") {
+                        if (error.error_id === 'INVALID_PIN') {
+                            console.log('The pin "' + answers.pin + '" is not valid');
+                        } else if (error.error_id === 'TIMEOUT') {
+                            console.log('Received a pairing session timeout');
+                        } else if (error.error_id === 'ECONNRESET') {
+                            console.log('The connection was reset during pairing, this shouldn\'t happen');
+                        } else {
+                            console.log('Unexpected pairing error', JSON.stringify(error));
+                        }
+                    } else {
+                        console.log('Unexpected pairing error', JSON.stringify(error));
+                    }
+                });
+            });
+        } else {
+            console.log('Concurrent pairing in progress');
+        }
+    };
+
+    let onRejectPair = (error) => {
+        if (typeof error.statusCode !== "undefined") {
+            if (error.statusCode === 404) {
+                console.log('Could not find api endpoint, got 404. Incorrect api version?');
+            } else {
+                console.log('Unexpected error\n', JSON.stringify(error));
+            }
+        } else if (typeof error.code !== "undefined") {
+            if (error.code === 'ECONNRESET') {
+                console.log('Could not reach api endpoint, connection was reset. Incorrect port number?');
+            } else {
+                console.log('Unexpected error\n', JSON.stringify(error));
+            }
+        } else {
+            console.log('Unexpected error\n', JSON.stringify(error));
+        }
+    };
+
+    let onResolveSystem = (response) => {
         let apiVersion = response.api_version.Major,
             systemFeatures = response.featuring.systemfeatures;
 
@@ -42,55 +110,12 @@ inquirer.prompt(questions).then(answers => {
             if (pairingType === "digest_auth_pairing") {
                 console.log('Starting pairing process');
 
-                client.startPair().then((response) => {
-                    if (response.error_id === 'SUCCESS') {
-                        console.log('Successfully started pairing process');
+                client.startPair().then(onResolvePair).catch(error => {
+                    console.log('Starting pair failed, trying different variations');
 
-                        inquirer.prompt([{
-                            'type': 'input',
-                            'name': 'pin',
-                            'message': 'What\'s the pin code displayed on the tv?'
-                        }]).then(answers => {
-                            console.log('Sending pin verification:', answers.pin);
+                    let pairingVariation = pairingVariations.pop();
 
-                            client.confirmPair(answers.pin).then((credentials) => {
-                                console.log('Pairing process completed successfully!');
-                                console.log('Your tv is now accessible with the following digest credentials', credentials);
-                            }).catch((error) => {
-                                if (typeof error.error_id !== "undefined") {
-                                    if (error.error_id === 'INVALID_PIN') {
-                                        console.log('The pin "' + answers.pin + '" is not valid');
-                                    } else if (error.error_id === 'TIMEOUT') {
-                                        console.log('Received a pairing session timeout');
-                                    } else if (error.error_id === 'ECONNRESET') {
-                                        console.log('The connection was reset during pairing, this shouldn\'t happen');
-                                    } else {
-                                        console.log('Unexpected pairing error', JSON.stringify(error));
-                                    }
-                                } else {
-                                    console.log('Unexpected pairing error', JSON.stringify(error));
-                                }
-                            });
-                        });
-                    } else {
-                        console.log('Concurrent pairing in progress');
-                    }
-                }).catch((error) => {
-                    if (typeof error.statusCode !== "undefined") {
-                        if (error.statusCode === 404) {
-                            console.log('Could not find api endpoint, got 404. Incorrect api version?');
-                        } else {
-                            console.log('Unexpected error\n', JSON.stringify(error));
-                        }
-                    } else if (typeof error.code !== "undefined") {
-                        if (error.code === 'ECONNRESET') {
-                            console.log('Could not reach api endpoint, connection was reset. Incorrect port number?');
-                        } else {
-                            console.log('Unexpected error\n', JSON.stringify(error));
-                        }
-                    } else {
-                        console.log('Unexpected error\n', JSON.stringify(error));
-                    }
+                    onPairingVariation(pairingVariation);
                 });
             } else {
                 console.log('Unexpected pairing type\n', JSON.stringify(response));
@@ -106,7 +131,9 @@ inquirer.prompt(questions).then(answers => {
                 console.log('Something went wrong\n', JSON.stringify(error));
             })
         }
-    }).catch((error) => {
+    };
+
+    let onRejectSystem = (error) => {
         if (typeof error !== "undefined" && error !== null) {
             if (typeof error.statusCode !== "undefined") {
                 if (error.statusCode === 404) {
@@ -128,5 +155,27 @@ inquirer.prompt(questions).then(answers => {
         } else {
             console.log('Unexpected error\n', JSON.stringify(error));
         }
-    });
+    };
+
+    let onPairingVariation = (pairingVariation) => {
+        client.setPort(pairingVariation.port);
+        client.setSecure(pairingVariation.secure);
+
+        console.log('Retrying pair on port ' + pairingVariation.port + ' with ' + (pairingVariation.secure ? 'https' : 'http'));
+
+        client.startPair().then(result => {
+            onResolvePair(result);
+        }).catch(error => {
+            console.log('Failed starting pair on port ' + pairingVariation.port + ' with ' + (pairingVariation.secure ? 'https' : 'http'));
+
+            if (pairingVariations.length > 0) {
+                onPairingVariation(pairingVariations.pop());
+            } else {
+                onRejectPair(error);
+            }
+        });
+    };
+
+    console.log('Sending system info call');
+    client.getSystem().then(onResolveSystem).catch(onRejectSystem);
 });
