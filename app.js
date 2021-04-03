c20f1a0019094b304914d9dd36f5e323cd2dd5e6
PiTRiS87 <pitris@gmail.com>
2021-04-03 13:38:45 +0200

    fix: initial volume fixes (#22)
    * Update .gitignore
    
    ignore node_modules folder
    
    * Simplify monitor loop
    
    Simplify the monitor loop and ensure it keeps running. So on/off detection wont stop.
    
    * Fixes volume settings:
    - +/-/mute buttons now work
    - slider works
    - buttons on slider do not work, but there is issue on other apps as well, so it might be homey issue
    - new settings to set maximum volume available on the tv (default 60 as per my testing tv)
    
    Co-authored-by: ElectiveRob <robstortelers1990@gmail.com>
    Co-authored-by: Lucas van der Have <lucasvdhave@gmail.com>

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 'use strict';
 
+if (process.env.DEBUG === '1') {
+    //require("inspector").open(9229, "0.0.0.0", false);
+    require('inspector').open(9229, "0.0.0.0");
+}
+
 const Homey = require('homey');
 
 class PhilipsTV extends Homey.App {
